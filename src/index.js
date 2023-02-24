f0fbdbea5e81023350a7c954dbe7b5dd49f1d3ac
Humberto Asca <hasca@ciplima.org.pe>
2023-02-24 15:49:02 -0500

    ADD: Getting a random fox image from the API.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,33 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+// console.log('Happy hacking :)')
+{/* <div class="p-4">
+    <img class="mx-auto" src="https://randomfox.ca/images/62.jpg" alt="Fox #62" width="320">
+</div> */}
+
+const max =122;
+const min =1;
+
+const random = () => Math.floor(Math.random()*(max-min))+ min;
+
+const createImageNode = ()=> {
+    const container = document.createElement('div');
+    container.className = 'p-4';
+
+    const imagen = document.createElement('img');
+    imagen.src =`https://randomfox.ca/images/${random()}.jpg` ; //todo
+    imagen.className="mx-auto";
+    imagen.width="320";
+    imagen.alt="Fox #62";
+
+    container.appendChild(imagen);
+
+    return container;
+}
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById('images')
+mountNode.appendChild(nuevaImagen);
+
+
